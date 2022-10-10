410dcdb440a156919dd8994e9bb1446acb86682a
vedesh1408 <102460188+vedesh1408@users.noreply.github.com>
2022-10-10 19:03:21 +1100

    Update main.py

--- CE-PiicoDev-VEML6030-MicroPython-Module-main/main.py
+++ CE-PiicoDev-VEML6030-MicroPython-Module-main/main.py
@@ -13,7 +13,7 @@ lightThreshold = 150
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-
+GPIO.output(LED_PIN, GPIO.LOW)
 while True:
     # Read and print light data
     lightVal = light.read()
